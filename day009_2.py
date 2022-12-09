dc98af24748b55e5e820aae29fa580c85e29704b
Oli Bishop <olib@olis-mbp.lan>
2022-12-09 15:22:26 +0000

    Clean up code, move some functions to common

--- day009_2.py
+++ day009_2.py
@@ -1,4 +1,4 @@
-from common import get_input
+from common import get_input, bcolors, get_new_grid
 
 # instructions = get_input(9)
 instructions = """R 5
@@ -10,10 +10,11 @@ D 10
 L 25
 U 20"""
 
+
 def move_head(direction, positions):
     curr = positions[0]
     if direction == 'U':
-        positions[0] = curr[0] -1, curr[1]
+        positions[0] = curr[0] - 1, curr[1]
     elif direction == 'R':
         positions[0] = curr[0], curr[1] + 1
     elif direction == 'D':
@@ -24,18 +25,20 @@ def move_head(direction, positions):
 
 
 def follow(positions, curr):
-    if curr > len(positions) -1:
+    if curr > len(positions) - 1:
         return
-    next = positions[curr -1]
+    next = positions[curr - 1]
     this = positions[curr]
     dx, dy = next[1] - this[1], next[0] - this[0]
 
     # check if we need to move at all
     if this == next:
         return
+    # next node is only 1 space from this
     if sum([abs(dx), abs(dy)]) == 1:
         return
-    if ((dx**2) + (dy**2)) == ((1**2) + (1**2)):
+    # next node is diagonally 1 away from this
+    if ((dx ** 2) + (dy ** 2)) == ((1 ** 2) + (1 ** 2)):
         return
 
     if dx != 0 and dy == 0:
@@ -48,54 +51,34 @@ def follow(positions, curr):
         # move diagonally
         positions[curr] = this[0] + (1 if dy > 0 else -1), this[1] + (1 if dx > 0 else -1)
 
-    follow(positions, curr +1)
+    follow(positions, curr + 1)
 
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+
 
 SIZE = 26
-def get_new_map():
-    seen_map = []
-    for _ in range(SIZE+1):
-        seen_map.append([f'{bcolors.OKGREEN}0'] * (SIZE+1))
-    return seen_map
-
 start = 12, 12
-positions = [start]*10
+positions = [start] * 10
 tail_positions = {positions[-1]}
 
 
 def draw_map(positions, tail_positions):
-    seen_map = get_new_map()
-    seen_map[start[0]][1] = "s"
-    print("----")
+    grid = get_new_grid(SIZE)
+    grid[start[0]][start[1]] = "s"
     for tail_position in tail_positions:
-        seen_map[tail_position[0]][tail_position[1]] = f"{bcolors.WARNING}#{bcolors.OKGREEN}"
+        grid[tail_position[0]][tail_position[1]] = f"{bcolors.WARNING}#{bcolors.OKGREEN}"
     for node in range(len(positions)):
         x, y = positions[node]
-        seen_map[x][y] = node if node != 0 else 'H'
-    for l in seen_map:
+        grid[x][y] = node if node != 0 else 'H'
+    print("----")
+    for l in grid:
         print(''.join([str(s) for s in l]))
 
-seen_map = get_new_map()
+
 for instruction in instructions.split('\n'):
     direction, num = instruction.split()[0], int(instruction.split()[1])
     for _ in range(num):
-        positions, seen_map = move_head(direction, positions)
+        positions = move_head(direction, positions)
         follow(positions, 1)
         tail_positions.add(positions[-1])
-draw_map(positions,tail_positions)
+draw_map(positions, tail_positions)
 print(len(tail_positions))
-
-
-
-
-
