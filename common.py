690b1f03d16943d6e2aeb359737265344e673399
Oli Bishop <olib@olis-mbp.lan>
2022-12-10 10:52:01 +0000

    Update grid funcs to be a bit more flexible, add use of colorama module

--- common.py
+++ common.py
@@ -1,6 +1,14 @@
+import colorama
+
+
 def get_input(day):
-    with open(f'config/day00{day}.txt', 'r') as f:
-        return f.read()
+    # this is a gross hack
+    if day > 9:
+        with open(f'config/day0{day}.txt', 'r') as f:
+            return f.read()
+    else:
+        with open(f'config/day00{day}.txt', 'r') as f:
+            return f.read()
 
 class bcolors:
     HEADER = '\033[95m'
@@ -13,8 +21,21 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
-def get_new_grid(size):
+def get_new_grid(width, height=0, default=0):
+    if height == 0:
+        height = width
+
     grid = []
-    for _ in range(size):
-        grid.append([0] * size)
+    for _ in range(height):
+        grid.append([default] * width)
     return grid
+
+def render_grid(grid, colour_dict):
+    print("----")
+    for row in grid:
+        for char in row:
+            if char in colour_dict:
+                print(colour_dict[char] + char + colorama.Style.RESET_ALL, end='')
+            else:
+                print(char, end='')
+        print('\n', end='')
