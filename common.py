e20b4b8fbe478d3fb3decb4d86721ceca3cc9821
Oli <oliverbishop90@gmail.com>
2022-12-10 12:50:53 +0000

    Remove unused bgcolors

--- common.py
+++ common.py
@@ -10,17 +10,6 @@ def get_input(day):
         with open(f'config/day00{day}.txt', 'r') as f:
             return f.read()
 
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-
 def get_new_grid(width, height=0, default=0):
     if height == 0:
         height = width
