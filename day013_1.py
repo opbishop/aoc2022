5f802e362185f8dfc0fd0cd8107090e98ab78c7d
Oli <oliverbishop90@gmail.com>
2022-12-13 12:46:21 +0000

    Clean up code

--- day013_1.py
+++ day013_1.py
@@ -9,33 +9,10 @@ from common import get_input, get_new_grid, render_grid
 from collections import defaultdict
 
 puzzle_input = get_input(13)
-# puzzle_input = """[1,1,3,1,1]
-# [1,1,5,1,1]
-#
-# [[1],[2,3,4]]
-# [[1],4]
-#
-# [9]
-# [[8,7,6]]
-#
-# [[4,4],4,4]
-# [[4,4],4,4,4]
-#
-# [7,7,7,7]
-# [7,7,7]
-#
-# []
-# [3]
-#
-# [[[]]]
-# [[]]
-#
-# [1,[2,[3,[4,[5,6,7]]]],8,9]
-# [1,[2,[3,[4,[5,6,0]]]],8,9]"""
 puzzle_input = puzzle_input.split('\n\n')
 
 
-def compare(left, right, top=False):
+def compare(left, right):
     if type(left) == int and type(right) == list:
         return compare([left], right)
     elif type(left) == list and type(right) == int:
@@ -67,6 +44,6 @@ for i, line_pair in enumerate(puzzle_input, 1):
     left, right = line_pair.split('\n')
     left = eval(left)
     right = eval(right)
-    if compare(left, right, True):
+    if compare(left, right):
         total.append(i)
 print(sum(total))
